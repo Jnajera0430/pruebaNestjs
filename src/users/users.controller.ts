bfb8484bb3dfd7db467782d8c8fb755f8bb1d468
Jose Najera <najerajoseantonio052@gmail.com>
2023-04-09 16:42:20 -0500

    Quedó listo el crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,34 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Delete, Patch } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
 
-    constructor( private userService: UsersService){}
+    constructor(private userService: UsersService) { }
     @Get()
-    getAllUsers(): Promise<User[]>{
+    getAllUsers(): Promise<User[]> {
         return this.userService.getUsers();
     }
 
     @Get(':id')
-    getUserById(@Param('id') id: string): Promise<User>{
+    getUserById(@Param('id') id: string): Promise<User> {
         return this.userService.getUser(id);
     }
 
     @Post()
-    createUser(@Body() newUser: CreateUserDto):Promise<User>{
+    createUser(@Body() newUser: CreateUserDto): Promise<User> {
         return this.userService.createUser(newUser);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string): Promise<User> | Object{
+        return this.userService.deleteUser(id);
+    }
+    @Patch(':id')
+    upDateUser(@Param('id') id: string, @Body() newUser: UpdateUserDto): Promise<User> | Object{
+        return this.userService.upDateUser(id, newUser); 
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import {User} from './users.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private userRepository: Repository<User>){}
@@ -22,5 +23,21 @@ export class UsersService {
                 id
             }
         })
+    }
+
+    deleteUser(id:string){
+        return this.userRepository.delete({id});
+    }
+
+    upDateUser(id: string, upDateUser: UpdateUserDto){
+        const findUser: Promise<User> = this.userRepository.findOne({
+            where: {
+                id
+            }
+        })
+        if(!findUser)return {
+            msg: 'Usuario no encontrado.'
+        }
+        return this.userRepository.update({id}, upDateUser);
     }
 }
